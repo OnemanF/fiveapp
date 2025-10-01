cf335d4949bed6384f12c366ca85aa3c8d9d23fa
OnemanF <feioen01@easv365.dk>
2025-10-01 20:52:36 +0200

    deploy

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,23 @@
+using System.Data.Common;
+using api;
+using efscaffold;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql(appOptions.DbConnectionString);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", ([FromServices] MyDbContext db) =>
+{   
+    var objects = db.Authors.ToList();
+});
+    
 app.Run();
